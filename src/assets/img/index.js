a1b9d858943f7fe8ad9d9d81bb51da77b57c236e
vuduchiieu <vuduchiieu@gmail.com>
2023-11-30 23:58:04 +0700

    fix bug

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -2,7 +2,6 @@ const img = {
     avt: require("./avt.jpg"),
     banner: require("./Hình ảnh.png"),
     phone: require("./phone.png"),
-    background: require("./background.png"),
     chauHong: require("./chau-hong.jpg"),
     chiDuoi: require("./chi-duoi.jpg"),
     chiTren: require("./chi-tren.jpg"),
