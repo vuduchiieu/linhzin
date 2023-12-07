0850ca65475f517531ca727d27ac6585460daf60
vuduchiieu <136909425+vuduchiieu@users.noreply.github.com>
2023-12-07 06:18:18 -0800

    Delete src directory

--- src/assets/icon/index.js
+++
@@ -1,8 +0,0 @@
-const icon = {
-    seach: require("./search.svg").default,
-    back: require("./Arrow - Left.svg").default,
-    top: require("./arrow - top.svg").default,
-    close: require("./Close Square.svg").default,
-};
-
-export default icon;
