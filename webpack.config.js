5f322d1fc80046e305f48d8267f982a79535482f
vuduchiieu <vuduchiieu@gmail.com>
2024-01-09 23:14:00 +0700

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: ["@babel/polyfill", path.resolve(__dirname, "src/index.js")],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.bundle.js",
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.svg$/,
+                use: ["svg-url-loader"],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "images/",
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "public", "index.html"),
+        }),
+    ],
+};
