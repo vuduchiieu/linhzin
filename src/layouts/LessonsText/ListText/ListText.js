b0ac46da39773038c59768729513ea6b7c427fbb
vuduchiieu <vuduchiieu@gmail.com>
2024-01-11 12:04:00 +0700

    demo text 2

--- src/layouts/LessonsText/ListText/ListText.js
+++ src/layouts/LessonsText/ListText/ListText.js
@@ -1,19 +1,38 @@
 import React from "react";
 
+import classNames from "classnames/bind";
+
+import styles from "./listText.module.scss";
+
+const cx = classNames.bind(styles);
+
 function ListText({ lessonsData }) {
     return (
-        <div
-            style={{ display: "flex", marginLeft: "600px", marginTop: "100px" }}
-        >
-            {lessonsData.map((item, i) => (
-                <iframe
-                    key={i}
-                    src={item.src}
-                    width="790"
-                    height="780"
-                    frameborder="0"
-                ></iframe>
-            ))}
+        <div className={cx("lessons")}>
+            <div className={cx("main")}>
+                <div className={cx("content")}>
+                    <div className={cx("video")}>
+                        <iframe
+                            allowFullScreen
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            src="https://drive.google.com/file/d/1LdxcPgcJ50FSOPbEg61B6fH7mjzlYndV/preview"
+                        ></iframe>
+                    </div>
+                    <div className={cx("title")}>
+                        <h3></h3>
+                    </div>
+                </div>
+
+                <div className={cx("nav-bar")}>
+                    <div className={cx("wrap")}>
+                        {lessonsData.map((item) => (
+                            <button key={item.id} className={cx("button")}>
+                                <p>{item.title}</p>
+                            </button>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
