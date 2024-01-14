751b42fbf8b9af9032c3ec26c1bdbe2e25323b5e
vuduchiieu <vuduchiieu@gmail.com>
2024-01-14 21:15:09 +0700

    demo text

--- src/layouts/LessonsText/ListText/ListText.js
+++ src/layouts/LessonsText/ListText/ListText.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import classNames from "classnames/bind";
 
 import styles from "./listText.module.scss";
+import { useAppContext } from "../../../components/context/AppContext";
 
 const cx = classNames.bind(styles);
 
 function ListText({ lessonsData }) {
+    const { handleOpen, selectedVideo, setActive, active } = useAppContext();
+    const activeButtonRef = useRef(null);
+
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true);
+        return () => {
+            if (isMounted) {
+                setActive(null);
+            }
+        };
+    }, [isMounted]);
+
+    useEffect(() => {
+        if (activeButtonRef.current) {
+            activeButtonRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [active, activeButtonRef]);
     return (
         <div className={cx("lessons")}>
             <div className={cx("main")}>
@@ -15,7 +34,7 @@ function ListText({ lessonsData }) {
                         <iframe
                             allowFullScreen
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            src="https://drive.google.com/file/d/1LdxcPgcJ50FSOPbEg61B6fH7mjzlYndV/preview"
+                            src={selectedVideo}
                         ></iframe>
                     </div>
                     <div className={cx("title")}>
@@ -26,7 +45,18 @@ function ListText({ lessonsData }) {
                 <div className={cx("nav-bar")}>
                     <div className={cx("wrap")}>
                         {lessonsData.map((item) => (
-                            <button key={item.id} className={cx("button")}>
+                            <button
+                                onClick={() =>
+                                    handleOpen(item.src, item.id, item.title)
+                                }
+                                key={item.id}
+                                className={cx("button", {
+                                    active: item.id === active,
+                                })}
+                                ref={
+                                    item.id === active ? activeButtonRef : null
+                                }
+                            >
                                 <p>{item.title}</p>
                             </button>
                         ))}
